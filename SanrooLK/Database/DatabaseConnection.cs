7087dfba82fee85e2114eac1379b5757fb9538bb
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-02-18 13:26:54 +0530

    Database Connection Created

---
+++ SanrooLK/Database/DatabaseConnection.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SanrooLK.Database
+{
+    internal class DatabaseConnection
+    {
+        private static string connectionString = "mongodb+srv://DesktopClient:6uJegLlg5cjVy5GS@sanroolk.xxwnk.mongodb.net/";
+        private static string databaseName = "SanrooLKDB";
+
+        public static IMongoDatabase GetDatabase()
+        {
+            var client = new MongoClient(connectionString);
+            return client.GetDatabase(databaseName);
+        }
+    }
+}
