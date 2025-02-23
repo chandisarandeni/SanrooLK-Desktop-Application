071581c509f2250409326d73350b165bc85bc68b
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-02-23 20:29:42 +0530

    Sales and Customer Service Design Part Done

--- SanrooLK/obj/Debug/net8.0-windows/Views/AdminOperations/Views/AdminViewProduct.g.i.cs
+++ SanrooLK/obj/Debug/net8.0-windows/Views/AdminOperations/Views/AdminViewProduct.g.i.cs
@@ -158,7 +158,7 @@ namespace SanrooLK.Views.AdminOperations.Views {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SanrooLK;V1.0.0.0;component/views/adminoperations/views/adminviewproduct.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SanrooLK;component/views/adminoperations/views/adminviewproduct.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\..\..\Views\AdminOperations\Views\AdminViewProduct.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
