4ec88dbf94e4080351eaca67a1f2524c0224bfab
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-02-23 12:44:02 +0530

    Admin View Product; Combo Box Created

--- SanrooLK/Views/AdminOperations/Views/AdminViewCustomer.xaml.cs
+++ SanrooLK/Views/AdminOperations/Views/AdminViewCustomer.xaml.cs
@@ -27,28 +27,28 @@ namespace SanrooLK.Views.AdminOperations.Views
 
         private void txt_customerID_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (txt_employeeID.Text == "Enter Employee ID or NIC to View Details")
+            if (txt_customerID.Text == "Enter Customer ID or NIC to View Details")
             {
-                txt_employeeID.Text = "";
-                txt_employeeID.Foreground = Brushes.Black;
+                txt_customerID.Text = "";
+                txt_customerID.Foreground = Brushes.Black;
             }
         }
 
         private void txt_customerID_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
         {
-            if (txt_employeeID.Text == "Enter Employee ID or NIC to View Details")
+            if (txt_customerID.Text == "Enter Customer ID or NIC to View Details")
             {
-                txt_employeeID.Text = "";
-                txt_employeeID.Foreground = Brushes.Black;
+                txt_customerID.Text = "";
+                txt_customerID.Foreground = Brushes.Black;
             }
         }
 
         private void txt_customerID_LostFocus(object sender, RoutedEventArgs e)
         {
-            if (string.IsNullOrWhiteSpace(txt_employeeID.Text))
+            if (string.IsNullOrWhiteSpace(txt_customerID.Text))
             {
-                txt_employeeID.Text = "Enter Employee ID or NIC to View Details";
-                txt_employeeID.Foreground = Brushes.Gray;
+                txt_customerID.Text = "Enter Employee ID or NIC to View Details";
+                txt_customerID.Foreground = Brushes.Gray;
             }
         }
     }
--- SanrooLK/Views/AdminOperations/Views/AdminViewProduct.xaml.cs
+++ SanrooLK/Views/AdminOperations/Views/AdminViewProduct.xaml.cs
@@ -24,5 +24,65 @@ namespace SanrooLK.Views.AdminOperations.Views
         {
             InitializeComponent();
         }
+
+        private void txt_productID_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_productID.Text == "Enter Product ID to View Details")
+            {
+                txt_productID.Text = "";
+                txt_productID.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txt_productID_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (txt_productID.Text == "Enter Product ID to View Details")
+            {
+                txt_productID.Text = "";
+                txt_productID.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txt_productID_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_productID.Text))
+            {
+                txt_productID.Text = "Enter Employee ID or NIC to View Details";
+                txt_productID.Foreground = Brushes.Gray;
+            }
+        }
+
+        private void TextBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Check which TextBlock was clicked and toggle the corresponding popup visibility
+            if (sender == SelectedOption)
+            {
+                OptionPopupCategory.IsOpen = !OptionPopupCategory.IsOpen;
+            }
+            else if (sender == SelectedOptionBrand)
+            {
+                OptionPopupBrand.IsOpen = !OptionPopupBrand.IsOpen;
+            }
+        }
+
+        private void OptionButton_Click(object sender, RoutedEventArgs e)
+        {
+            var button = sender as Button;
+            if (button == null) return;
+
+            if (OptionPopupCategory.IsOpen)
+            {
+                SelectedOption.Text = button.Content.ToString();
+                OptionPopupCategory.IsOpen = false;
+            }
+            else if (OptionPopupBrand.IsOpen)
+            {
+                SelectedOptionBrand.Text = button.Content.ToString();
+                OptionPopupBrand.IsOpen = false;
+            }
+        }
+
+
+
     }
 }
