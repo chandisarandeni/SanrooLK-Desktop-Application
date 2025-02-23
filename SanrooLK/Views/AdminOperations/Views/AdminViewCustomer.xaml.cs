c37555f256163f2e79d93f00a92f1c59aeca20de
Chandisa Randeni <chandisarandeni2003@gmail.com>
2025-02-23 09:34:06 +0530

    Admin View Customer; Design Part Done

--- SanrooLK/Views/AdminOperations/Views/AdminViewCustomer.xaml.cs
+++ SanrooLK/Views/AdminOperations/Views/AdminViewCustomer.xaml.cs
@@ -24,5 +24,32 @@ namespace SanrooLK.Views.AdminOperations.Views
         {
             InitializeComponent();
         }
+
+        private void txt_customerID_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (txt_employeeID.Text == "Enter Employee ID or NIC to View Details")
+            {
+                txt_employeeID.Text = "";
+                txt_employeeID.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txt_customerID_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            if (txt_employeeID.Text == "Enter Employee ID or NIC to View Details")
+            {
+                txt_employeeID.Text = "";
+                txt_employeeID.Foreground = Brushes.Black;
+            }
+        }
+
+        private void txt_customerID_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txt_employeeID.Text))
+            {
+                txt_employeeID.Text = "Enter Employee ID or NIC to View Details";
+                txt_employeeID.Foreground = Brushes.Gray;
+            }
+        }
     }
 }
